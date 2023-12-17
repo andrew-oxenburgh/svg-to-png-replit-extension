0fa78ddf7c902a4afb3b19198ea11a5008e6abc4
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2023-12-17 06:44:17 +0000

    almost!!!

--- src/tool/Root.tsx
+++ src/tool/Root.tsx
@@ -1,7 +1,7 @@
 import { HandshakeProvider } from "@replit/extensions-react";
 import { createRoot } from "react-dom/client";
 import { useReplit } from "@replit/extensions-react";
-
+import SvgToPng from "./SvgToPng";
 function App() {
   const { status, error, replit } = useReplit();
 
@@ -13,14 +13,11 @@ function App() {
     return <div>An error occurred: {error?.message}</div>;
   }
 
-  return <div>Extension is Ready!</div>;
+  return <SvgToPng />;
 }
 
 createRoot(document.getElementById("root")).render(
   <HandshakeProvider>
     <App />
-  </HandshakeProvider>
+  </HandshakeProvider>,
 );
-
-render(<Component />, document.getElementById("root") as Element);
-
---
+++ src/tool/SvgToPng.tsx
@@ -0,0 +1,142 @@
+import { useEffect, useState, useRef, Fragment } from "react";
+import * as utils from "./utils";
+import { render } from "react-dom";
+import { useReplit } from "@replit/extensions-react";
+import { fs } from "@replit/extensions";
+
+import {
+  Header,
+  Container,
+  Image,
+  Input,
+  Button,
+  Form,
+  Segment,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./override.css";
+
+function Mono({ children }) {
+  return <code className="mono">{children}</code>;
+}
+
+function Page({ children }) {
+  return <Container className="page">{children}</Container>;
+}
+
+enum SavingState {
+  saved,
+  saving,
+  error,
+}
+
+function SvgToPng() {
+  const [srcFile, setSrcName] = useState<string>("examples/1.svg");
+  const [destFile, setDestFile] = useState<string>("icons/icon-200x200.png");
+  const [fileFound, setFileFound] = useState<boolean>(true);
+  const [saving, setSaving] = useState<SavingState>(SavingState.saved);
+  const [side, setSide] = useState<number>(200);
+
+  useEffect(() => {
+    const run = async () => {
+      const imgEle = document.querySelector("#img") as HTMLImageElement;
+      if (!fileFound) {
+        imgEle.src = utils.dataUrlBlank;
+        return;
+      }
+      const { error, content } = await fs.readFile(srcFile);
+      if (error) {
+        return;
+      }
+      const enc = utils.svgEncode(content);
+
+      imgEle.src = enc;
+    };
+    run();
+  }, [fileFound]);
+
+  const onRead = async () => {
+    setSaving(SavingState.saving);
+    // can't use ref's with semantic-ui-react
+    const imgEle = document.querySelector("#img") as HTMLImageElement;
+    const canvasEle = document.querySelector("#canvas") as HTMLCanvasElement;
+    imgEle.onload = async () => {
+      const ctx = canvasEle.getContext("2d");
+      ctx.fillStyle = "orange";
+      ctx.fillRect(0, 0, canvasEle.width, canvasEle.height);
+      ctx.drawImage(imgEle, 0, 0, side, side);
+      const data = canvasEle.toDataURL("image/png)");
+      utils.saveTo(destFile, data);
+      setSaving(SavingState.saved);
+    };
+
+    const { error, content } = await fs.readFile(srcFile);
+    if (error) {
+      alert("error reading file");
+      return;
+    }
+    const enc = utils.svgEncode(content);
+
+    imgEle.src = enc;
+  };
+
+  const onFileNameChange = async (e: any) => {
+    const newFileName = e.target.value;
+    setSrcName(newFileName);
+    const { error } = await fs.readFile(newFileName);
+    const found = !(error && error.length > 0);
+    setFileFound(found);
+  };
+
+  const changeSide = (n: number) => {
+    setSide(n);
+    setDestFile(`icons/icon-${n}x${n}.png`);
+  };
+
+  const onChangeSide = (e: any) => {
+    const sz = e.target.value;
+    changeSide(parseInt(sz));
+  };
+
+  let buttonStatus = fileFound ? { primary: true } : { disabled: true };
+  // const disabled = "";
+  return (
+    <Page>
+      <Header textAlign="centered" size="huge">
+        svg-to-png
+      </Header>
+      <Segment attached>
+        <p>
+          Select an svg source file, and a size and it will generate that png
+          for in the <Mono>icons</Mono> folder.
+        </p>
+        <p>It will overwrite anything already in there, so be warned.</p>
+      </Segment>
+
+      <Header center size="large"></Header>
+      <Header size="small">Create png icons from an svg</Header>
+      <Form>
+        <Form.Field>
+          <label>Enter filename</label>
+          <Input type="text" onChange={onFileNameChange} value={srcFile} />
+        </Form.Field>
+        <Form.Field>
+          <label>icon size</label>
+          <Input type="number" onChange={onChangeSide} value={side} />
+        </Form.Field>
+        <p className={saving === SavingState.saving ? "saving" : "saved"}>
+          saving to <Mono>{destFile}</Mono>
+        </p>
+        <Button onClick={onRead} {...buttonStatus}>
+          Create Icon
+        </Button>
+      </Form>
+      <Image id="img" size="small" centered />
+      <canvas id="canvas" width={side} height={side} />
+    </Page>
+  );
+}
+
+render(<SvgToPng />, document.getElementById("root") as Element);
+
+export default SvgToPng;
